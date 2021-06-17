d7e6c1d06e8283e8edcf97866389931205cbdb4b
MattieIshida <liquidabomination@protonmail.com>
2021-06-17 21:07:53 +0500

    В отдельные методы выделены: неполная матрица истинности, precision, recall, beta f measure, их альтернативные варианты подсчёта и подсчёт всех метрик скопом

--- main.py
+++ main.py
@@ -2,95 +2,129 @@ import requests
 import re
 import io
 from collections import defaultdict
-from datetime import datetime
-import mysql.connector
-from mysql.connector import Error
+from datetime import datetime, date, time
 from python_mysql_connect1 import *
 from telegram_notify import send_notification
-import timeit
 from pprint import pprint
+import timeit
 
 
-# Запрос статистики за конкретный период
-def import_data():
+def import_data():  # Запрос статистики за конкретный период
     headers = {"Content-Type": "application/json", "X-Auth-Token": "4CE7B412-49B7-3DCF-B56D-3441B6A3698A"}
     url = 'http://localhost:8080/execmodel'
-    dates = {'start': '01.01.2018', 'finish': '01.06.2019'}
+    dates = {'start': '01.01.2010', 'finish': '31.12.2021'}
     urlData = requests.post(url, json=dates, headers=headers)
     testData = io.StringIO(urlData.json())
     parsed_data = re.findall(r'\w+', testData.readline().replace('\n', ''))
+    testData.close()
+    urlData.close()
     return parsed_data
 
 
-# Подготовка данных
-def dict_inn(raw_data):
-    dictInns = defaultdict(list)  # Пустой словарь с ключами из INN
+def raw_dictionary(raw_data):  # Подготовка данных
+    dictionary = defaultdict(list)  # Пустой словарь с ключами из INN
     for i in range(0, len(raw_data), 6):
         if i == len(raw_data):
             break
         else:
-            dictInns.setdefault(int(int(raw_data[i + 3])), []).append(int(int(raw_data[i + 1]) / 1000))
-            dictInns.setdefault(int(raw_data[i + 3]), []).append(raw_data[i + 5])
+            dictionary.setdefault(int(int(raw_data[i + 3])), []).append(int(int(raw_data[i + 1]) / 1000))
+            dictionary.setdefault(int(raw_data[i + 3]), []).append(raw_data[i + 5])
             i += 6
-    dictInns = dict(dictInns)
-    return dictInns
+    dictionary = dict(dictionary)
+    return dictionary
 
 
-# dictInns = dict(dictInns)
+def confusion_matrix(dictionary):   # Получение индексов неполной матрицы истинности
+    tp = 0
+    fp = 0
+    fn = 0
+    conversion_incomplete = 0
+    for key in dict(dictionary):
+        if 'PURCHASE' in dictionary[key] and 'CONVERSION' in dictionary[key]:
+            tp += 1
+        if 'PURCHASE' in dictionary[key] and 'CONVERSION' not in dictionary[key]:
+            fn += 1
+        if 'CLOSE' in dictionary[key]:
+            fp += 1
+        if 'CONVERSION' in dictionary[key] and 'PURCHASE' not in dictionary[key]:
+            conversion_incomplete += 1
+        if (len(dictionary[key]) == 2) and (dictionary[key][1] == 'RECOMENDATION'):
+            fp += 1
+        key += 1
+    tp_alt = tp + conversion_incomplete
+    fp_alt = fp + conversion_incomplete
+    return tp, tp_alt, fp, fp_alt, fn
 
 
-# На выходе получим словарь dictDates формата:
-# { datetime:[inn, действие],
-#   datetime:[inn, действие], ...}
+def get_precision(indexes):  # Подсчёт точности
+    precision = indexes[0] / (indexes[0] + indexes[3])
+    return precision
 
-# И ещё один словарь dictInns формата:
-# { inn:[datetime, действие, datetime2, действие2],
-#   inn:[datetime, действие], ...}
-# pprint(dictDates)
-# pprint(dictInns)
 
-# TP = 0     # Учёт выборки True Positive
-# FP = 0     # Учёт выборки False Positive
-# FN = 0
-# TP_alt = 0
-# FP_alt = 0
-# ConversionAlt = 0
-#
-#
-# for key in dict(dictInns):
-#     if 'PURCHASE' in dictInns[key] and 'CONVERSION' in dictInns[key]:
-#         TP += 1
-#     if 'PURCHASE' in dictInns[key] and 'CONVERSION' not in dictInns[key]:
-#         FN += 1
-#     if 'CLOSE' in dictInns[key]:
-#         FP += 1
-#     if 'CONVERSION' in dictInns[key] and 'PURCHASE' not in dictInns[key]:
-#         ConversionAlt += 1
-#     if (len(dictInns[key]) == 2) and (dictInns[key][1] == 'RECOMENDATION'):
-#         FP += 1
-#     key += 1
-#
-# TP_alt = TP + ConversionAlt
-# FP_alt = FP + ConversionAlt
-# beta = 1
-#
-# Precision = TP / (TP + FP)  # Precision
-# PrecisionAlt = TP_alt / (TP_alt + FP_alt)
-# Recall = TP / (TP + FN)
-# RecallAlt = TP_alt / (TP_alt + FN)
-# BetaFMeasure = (1 + beta*beta) * Precision * Recall / ((beta*beta*Precision) + Recall)
-# BetaFMeasureAlt = (1 + beta*beta) * PrecisionAlt * RecallAlt / ((beta*beta*PrecisionAlt) + RecallAlt)
-#
-# arguments = [TP, FP, Precision, PrecisionAlt, Recall, RecallAlt, BetaFMeasure, BetaFMeasureAlt]
-#
-# summary = ("\nTP: {}\n"
-#            "FP: {}\n"
-#            "Precision (Точность): {}\n"
-#            "Precision alternative: {}\n"
-#            "Recall (Полнота): {}\n"
-#            "Recall alternative: {}\n"
-#            "F-Мера: {}\n"
-#            "F-Мера alternative: {}\n").format(*arguments)
+def get_precision_alt(indexes):  # Альтернативный подсчёт точности
+    precision = indexes[1] / (indexes[1] + indexes[2])
+    return precision
+
+
+def get_recall(indexes):    # Подсчёт полноты
+    recall = indexes[0] / (indexes[0] + indexes[4])
+    return recall
+
+
+def get_recall_alt(indexes):    # Альтернавтивный подсчёт полноты
+    recall_alt = indexes[1] / (indexes[1] + indexes[4])
+    return recall_alt
+
+
+def get_beta_f_measure(indexes, beta):  # Подсчёт F-меры
+    BetaFMeasure = (1 + beta * beta) * \
+                   (indexes[0] / (indexes[0] + indexes[3])) * \
+                   (indexes[0] / (indexes[0] + indexes[4])) / \
+                   ((beta * beta * (indexes[0] / (indexes[0] + indexes[3]))) +
+                    (indexes[0] / (indexes[0] + indexes[4])))
+    return BetaFMeasure
+
+
+def get_beta_f_measure_alt(indexes, beta):  # Альтернативный подсчёт F-меры
+    BetaFMeasureAlt = (1 + beta * beta) * \
+                      (indexes[1] / (indexes[1] + indexes[2])) * (indexes[1] / (indexes[1] + indexes[4])) \
+                      / ((beta * beta * (indexes[1] / (indexes[1] + indexes[2]))) +
+                         (indexes[1] / (indexes[1] + indexes[4])))
+    return BetaFMeasureAlt
+
+
+def get_metrics(tp, tp_alt, fp, fp_alt, fn, beta):  # Подсчёт всех метрик разом
+    Precision = tp / (tp + fp_alt)
+    PrecisionAlt = tp_alt / (tp_alt + fp)
+    Recall = tp / (tp + fn)
+    RecallAlt = tp_alt / (tp_alt + fn)
+    BetaFMeasure = (1 + beta*beta) * Precision * Recall / ((beta*beta*Precision) + Recall)
+    BetaFMeasureAlt = (1 + beta*beta) * PrecisionAlt * RecallAlt / ((beta*beta*PrecisionAlt) + RecallAlt)
+    return Precision, PrecisionAlt, Recall, RecallAlt, BetaFMeasure, BetaFMeasureAlt
+
+
+def get_summary(indexes, metrics):  # Сводка по всем данным
+    summary = ("\nTP: {}\n"
+               "FP: {}\n"
+               "Оборванных на Conversion цепочек: {}\n"
+               "Precision: {}\n"
+               "Precision alternative: {}\n"
+               "Recall: {}\n"
+               "Recall alternative: {}\n"
+               "F-Мера: {}\n"
+               "F-Мера alternative: {}\n").format(indexes[0], indexes[2], (indexes[1]-indexes[0]),
+                                                  *metrics)
+    return summary
+
+
+def main():
+    timestamp = datetime.now()
+    indexes = confusion_matrix(raw_dictionary(import_data()))
+    metrics = get_metrics(indexes[0], indexes[1], indexes[2], indexes[3], indexes[4], 1)
+    summary = get_summary(indexes, metrics)
+    send_notification(summary, timestamp)
+
 
 if __name__ == '__main__':
-    pprint(dict_inn(import_data()))
+    main()
+
--- telegram_notify.py
+++ telegram_notify.py
@@ -3,7 +3,7 @@ import requests
 
 def send_notification(self, datetime):
     API_link = "https://api.telegram.org/bot1885420798:AAFlz0IBlmzylgLaR9Iurkx8S1r7GX8hGLQ"
-    text = "Сводка за {}, \n {}".format(datetime, self)
+    text = "Тестовая сводка за {} // \n {}".format(datetime, self)
     chat_id = -495488219
     requests.get(API_link + f"/sendMessage?chat_id={chat_id}&text={text}")
 
