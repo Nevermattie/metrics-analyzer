350efbe8e5c9d205194e2228876705a2502d1e3a
MattieIshida <liquidabomination@protonmail.com>
2021-06-17 18:57:59 +0500

    Импорт и подготовка данных выделены в отдельные методы

--- main.py
+++ main.py
@@ -7,34 +7,36 @@ import mysql.connector
 from mysql.connector import Error
 from python_mysql_connect1 import *
 from telegram_notify import send_notification
+import timeit
+from pprint import pprint
+
 
 # Запрос статистики за конкретный период
-headers = {"Content-Type": "application/json", "X-Auth-Token": "4CE7B412-49B7-3DCF-B56D-3441B6A3698A"}
-url = 'http://localhost:8080/execmodel'
-dates = {'start': '01.01.2018', 'finish': '01.06.2019'}
-urlData = requests.post(url, json=dates, headers=headers)
-testData = io.StringIO(urlData.json())
-testParse = re.findall(r'\w+', testData.readline().replace('\n', ''))
-
-dictDates = defaultdict(list)  # Пустой словарь с ключами из datetime
-dictInns = defaultdict(list)   # Пустой словарь с ключами из INN
-
-
-connect()
+def import_data():
+    headers = {"Content-Type": "application/json", "X-Auth-Token": "4CE7B412-49B7-3DCF-B56D-3441B6A3698A"}
+    url = 'http://localhost:8080/execmodel'
+    dates = {'start': '01.01.2018', 'finish': '01.06.2019'}
+    urlData = requests.post(url, json=dates, headers=headers)
+    testData = io.StringIO(urlData.json())
+    parsed_data = re.findall(r'\w+', testData.readline().replace('\n', ''))
+    return parsed_data
 
 
-i = 1
-while i <= len(testParse):
-    # Проверяет наличие ключа-inn в dictInns с элементом-списком и добавляет в список datetime
-    dictInns.setdefault(int(int(testParse[i + 2])), []).append(int(int(testParse[i])/1000))
+# Подготовка данных
+def dict_inn(raw_data):
+    dictInns = defaultdict(list)  # Пустой словарь с ключами из INN
+    for i in range(0, len(raw_data), 6):
+        if i == len(raw_data):
+            break
+        else:
+            dictInns.setdefault(int(int(raw_data[i + 3])), []).append(int(int(raw_data[i + 1]) / 1000))
+            dictInns.setdefault(int(raw_data[i + 3]), []).append(raw_data[i + 5])
+            i += 6
+    dictInns = dict(dictInns)
+    return dictInns
 
-    # Проверяет наличие ключа-inn в dictInns с элементом-списком и добавляет в список действие
-    dictInns.setdefault(int(testParse[i + 2]), []).append(testParse[i + 4])
-    print(i, " ", len(testParse))
-    i += 6
 
-
-dictInns = dict(dictInns)
+# dictInns = dict(dictInns)
 
 
 # На выходе получим словарь dictDates формата:
@@ -47,41 +49,48 @@ dictInns = dict(dictInns)
 # pprint(dictDates)
 # pprint(dictInns)
 
-TP = 0     # Учёт выборки True Positive
-FP = 0     # Учёт выборки False Positive
-FN = 0
-ConversionAlt = 0
+# TP = 0     # Учёт выборки True Positive
+# FP = 0     # Учёт выборки False Positive
+# FN = 0
+# TP_alt = 0
+# FP_alt = 0
+# ConversionAlt = 0
+#
+#
+# for key in dict(dictInns):
+#     if 'PURCHASE' in dictInns[key] and 'CONVERSION' in dictInns[key]:
+#         TP += 1
+#     if 'PURCHASE' in dictInns[key] and 'CONVERSION' not in dictInns[key]:
+#         FN += 1
+#     if 'CLOSE' in dictInns[key]:
+#         FP += 1
+#     if 'CONVERSION' in dictInns[key] and 'PURCHASE' not in dictInns[key]:
+#         ConversionAlt += 1
+#     if (len(dictInns[key]) == 2) and (dictInns[key][1] == 'RECOMENDATION'):
+#         FP += 1
+#     key += 1
+#
+# TP_alt = TP + ConversionAlt
+# FP_alt = FP + ConversionAlt
+# beta = 1
+#
+# Precision = TP / (TP + FP)  # Precision
+# PrecisionAlt = TP_alt / (TP_alt + FP_alt)
+# Recall = TP / (TP + FN)
+# RecallAlt = TP_alt / (TP_alt + FN)
+# BetaFMeasure = (1 + beta*beta) * Precision * Recall / ((beta*beta*Precision) + Recall)
+# BetaFMeasureAlt = (1 + beta*beta) * PrecisionAlt * RecallAlt / ((beta*beta*PrecisionAlt) + RecallAlt)
+#
+# arguments = [TP, FP, Precision, PrecisionAlt, Recall, RecallAlt, BetaFMeasure, BetaFMeasureAlt]
+#
+# summary = ("\nTP: {}\n"
+#            "FP: {}\n"
+#            "Precision (Точность): {}\n"
+#            "Precision alternative: {}\n"
+#            "Recall (Полнота): {}\n"
+#            "Recall alternative: {}\n"
+#            "F-Мера: {}\n"
+#            "F-Мера alternative: {}\n").format(*arguments)
 
-for key in dict(dictInns):
-    if 'PURCHASE' in dictInns[key] and 'CONVERSION' in dictInns[key]:
-        TP += 1
-    if 'PURCHASE' in dictInns[key] and 'CONVERSION' not in dictInns[key]:
-        FN += 1
-    if 'CLOSE' in dictInns[key]:
-        FP += 1
-    if 'CONVERSION' in dictInns[key] and 'PURCHASE' not in dictInns[key]:
-        ConversionAlt += 1
-    if (len(dictInns[key]) == 2) and (dictInns[key][1] == 'RECOMENDATION'):
-        FP += 1
-    key += 1
-
-beta = 1
-Precision = (TP + ConversionAlt) / (TP + ConversionAlt + FP)  # Precision
-PrecisionAlt = TP / (TP + FP + ConversionAlt)
-Recall = TP / (TP + FN)
-RecallAlt = (TP + ConversionAlt) / (TP + FN + ConversionAlt)
-BetaFMeasure = (1 + beta*beta) * Precision * Recall / ((beta*beta*Precision) + Recall)
-
-arguments = [TP, FP, Precision, PrecisionAlt, Recall, RecallAlt, BetaFMeasure]
-
-summary = ("\nTP: {}\n"
-           "FP: {}\n"
-           "Precision (Точность): {}\n"
-           "Precision alternative: {}\n"
-           "Recall (Полнота): {}\n"
-           "Recall alternative: {}\n"
-           "F-Мера: {}").format(*arguments)
-
-
-
-
+if __name__ == '__main__':
+    pprint(dict_inn(import_data()))
--- main2.py
+++ main2.py
@@ -10,18 +10,6 @@ from python_mysql_connect1 import *
 from python_mysql_dbconfig import *
 
 
-# Запрос статистики за конкретный период
-headers = {"Content-Type": "application/json", "X-Auth-Token": "4CE7B412-49B7-3DCF-B56D-3441B6A3698A"}
-url = 'http://localhost:8080/execmodel'
-dates = {'start': '01.01.2018', 'finish': '01.06.2019'}
-urlData = requests.post(url, json=dates, headers=headers)
-testData = io.StringIO(urlData.json())
-testParse = re.findall(r'\w+', testData.readline().replace('\n', ''))
-
-dictDates = defaultdict(list)  # Пустой словарь с ключами из datetime
-dictInns = defaultdict(list)   # Пустой словарь с ключами из INN
-
-
 # mydb = mysql.connector.connect(host='{}'.format("localhost"),   database='{}'.format("db_metrics"),
 #                                user='{}'.format("root"),        password='{}'.format("password"))
 
